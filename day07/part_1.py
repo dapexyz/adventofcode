857b34e22d1d98950a1e6ae1ebaf3b508d7dd6f5
dapexyz <dape.xyz@gmail.com>
2024-12-07 11:54:57 +0100

    ✅ Day 7

---
+++ day07/part_1.py
@@ -0,0 +1,30 @@
+import itertools
+
+with open(0) as f:
+    lines = f.read().strip().splitlines()
+
+t = 0
+
+operators = ['+', '*']
+
+for line in lines:
+    target_result, numbers = line.split(': ')
+    target_result = int(target_result)
+    numbers = list(map(int, numbers.split()))
+
+    for possible_operator_combination in itertools.product(operators, repeat=len(numbers) - 1):
+        result = numbers[0]
+
+        for i in range(1, len(numbers)):
+            operator = possible_operator_combination[i - 1]
+            if operator == '*':
+                result *= numbers[i]
+            else:
+                result += numbers[i]
+
+        if result == target_result:
+            t += target_result
+            break
+
+print(t)
+
